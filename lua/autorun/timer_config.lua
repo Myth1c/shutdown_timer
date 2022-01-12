85ed6d81d601acb93cc372101cb89b38227fa7f4
Myth1c <Alemsm1@gmail.com>
2022-01-12 00:31:50 -0500

    Fixed wrong defaults

--- lua/autorun/timer_config.lua
+++ lua/autorun/timer_config.lua
@@ -10,10 +10,10 @@ Timer_Config = {
     -- Debug code to allow console prints on server & client (Default 0)
     TIMER_DEBUG = { ENABLED = 0 },
     -- How long in seconds the timer takes to complete (Default 30)
-    TIMER_Time = 10,
+    TIMER_Time = 30,
     -- How often the server will check to update clients (Default 1)
     THINK_Delay = 1,
-    -- Wether or not the server should give an error message to the clients if the server doesn't restart
+    -- Wether or not the server should give an error message to the clients if the server doesn't restart (Default True)
     ERROR_Message = true,
 
 }
